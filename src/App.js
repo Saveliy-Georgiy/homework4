8cd48fee5110bd1636eb6f6c744eacdfd1f3333d
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-26 15:12:21 +0300

    Applied common code style to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import MyName from "./components/MyName/MyName";
 
 class App extends React.Component {
