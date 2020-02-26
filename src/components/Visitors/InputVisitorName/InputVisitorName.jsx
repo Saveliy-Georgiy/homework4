1f5d13401798ebcefb3a9f7500bc312a3f744e87
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-26 17:28:48 +0300

    Create component InputVisitorName template

---
+++ src/components/Visitors/InputVisitorName/InputVisitorName.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./InputVisitorName.module.css";
+
+class InputVisitorName extends React.Component {
+
+    render = () => {
+        return (
+            <div className={styles.inputVisitorName}>
+                //todo Logic
+            </div>
+        );
+    }
+}
+
+export default InputVisitorName;
--- src/components/Visitors/Visitors.jsx
+++ src/components/Visitors/Visitors.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import styles from "./Visitors.module.css";
 import CounterVisitors from "./CounterVisitors/CounterVisitors";
+import InputVisitorName from "./InputVisitorName/InputVisitorName";
 
 class Visitors extends React.Component {
 
     render = () => {
         return (
             <div className={styles.visitors}>
-                //todo Add 3 components: InputVisitorName, AddVisitor, HelloVisitor.
+                //todo Add 2 components: AddVisitor, HelloVisitor.
                 <CounterVisitors/>
+                <InputVisitorName/>
             </div>
         );
     }
