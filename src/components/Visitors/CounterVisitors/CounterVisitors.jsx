a51f69152d243c4490879061c47573c4cfdaa7ef
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-26 17:21:54 +0300

    Create component CounterVisitors template

---
+++ src/components/Visitors/CounterVisitors/CounterVisitors.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./CounterVisitors.module.css";
+
+class CounterVisitors extends React.Component {
+
+    render = () => {
+        return (
+            <div className={styles.counterVisitors}>
+                //todo Logic
+            </div>
+        );
+    }
+}
+
+export default CounterVisitors;
--- src/components/Visitors/Visitors.jsx
+++ src/components/Visitors/Visitors.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./Visitors.module.css";
+import CounterVisitors from "./CounterVisitors/CounterVisitors";
 
 class Visitors extends React.Component {
 
     render = () => {
         return (
             <div className={styles.visitors}>
-                //todo Add 4 components: CounterVisitors, InputVisitorName, AddVisitor, HelloVisitor.
+                //todo Add 3 components: InputVisitorName, AddVisitor, HelloVisitor.
+                <CounterVisitors/>
             </div>
         );
     }
