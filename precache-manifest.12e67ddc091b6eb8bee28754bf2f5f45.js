fc4fb27d916a6459f83a5ecb1d7cfe92e2ced359
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-28 23:34:13 +0300

    Updates

---
+++ precache-manifest.12e67ddc091b6eb8bee28754bf2f5f45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88e6637ebffa5819919b158aa2ac568",
+    "url": "/homework4/index.html"
+  },
+  {
+    "revision": "ce5cb5251b71b525460d",
+    "url": "/homework4/static/css/main.16e2278d.chunk.css"
+  },
+  {
+    "revision": "6850a0a2fb1248d49b68",
+    "url": "/homework4/static/js/2.b7469c75.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/homework4/static/js/2.b7469c75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce5cb5251b71b525460d",
+    "url": "/homework4/static/js/main.e69a2fe2.chunk.js"
+  },
+  {
+    "revision": "8753e0984484fbf91b9e",
+    "url": "/homework4/static/js/runtime-main.2624eab4.js"
+  },
+  {
+    "revision": "ede20697de9c59b4087b760a5489832d",
+    "url": "/homework4/static/media/background.ede20697.jpg"
+  }
+]);
