6a419d5176461a72d13f7ab6b79f966163d6ab8a
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-02-28 13:14:18 +0300

    Add styles for App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,17 @@ import MyName from "./components/MyName/MyName";
 import MyQualities from "./components/MyQualities/MyQualities";
 import Visitors from "./components/Visitors/Visitors";
 
+
 class App extends React.Component {
 
     render = () => {
         return (
             <div className="App">
+                <div className="mainContainer">
                 <MyName/>
                 <MyQualities/>
                 <Visitors/>
+                </div>
             </div>
         );
     };
