ad5a1ae2683c6ab353a33c6324dd81ade884cd07
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-03-03 15:08:06 +0300

    Create adaptation for iphone 5 and changed App.css into App.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
 import MyName from "./components/MyName/MyName";
 import MyQualities from "./components/MyQualities/MyQualities";
 import Visitors from "./components/Visitors/Visitors";
@@ -9,8 +9,9 @@ class App extends React.Component {
 
     render = () => {
         return (
-            <div className="App">
-                <div className="mainContainer">
+            <div className={styles.App}>
+                <meta name="viewport" content="width=device-width; initial-scale=0.85; maximum-scale=0.85; user-scalable=0;" />
+                <div className={styles.mainContainer}>
                 <MyName/>
                 <MyQualities/>
                 <Visitors/>
