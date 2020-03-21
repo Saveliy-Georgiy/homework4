a588d4c9792f969a4d4cd7d8a311235f2ff87355
Saveliy-Georgiy <savvabiryukov1@gmail.com>
2020-03-21 15:23:50 +0300

    Add loading page

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,52 @@ import Navbar from "./components/Monday/Navbar/Navbar";
 import Monday from "./components/Monday/Monday";
 import {HashRouter, Route} from "react-router-dom";
 import Tuesday from "./components/Tuesday/Tuesday";
+import {restoreState} from "./localStorage/localStorage";
+import Loading from "./components/Loading/Loading";
 
 
 class App extends React.Component {
 
+    componentDidMount() {
+        setTimeout(this.setTime, 3000);
+        let state = restoreState("App-state", this.state);
+        this.setState(state);
+    }
+
     state = {
         dayOfWeek: [
-            {title: "monday", link:"/monday", style: "styles.activeLink"},
-            {title: "tuesday", link:"/tuesday", style: "styles.activeLink"},
+            {title: "monday", link: "/monday", style: "styles.activeLink"},
+            {title: "tuesday", link: "/tuesday", style: "styles.activeLink"},
         ],
+        loading: true,
+    };
+
+    setTime = () => {
+        this.setState({
+            loading: false
+        });
     };
 
     render = () => {
+        if (this.state.loading) {
         return (
-            <HashRouter>
-                <div className={styles.App}>
-                    <Navbar dayOfWeek={this.state.dayOfWeek}/>
-                    <Route path="/monday"
-                           render={() => <Monday/>}/>
-                    <Route path="/tuesday"
-                           render={() => <Tuesday/>}/>
-                </div>
-            </HashRouter>);
+            <div className={styles.App}>
+            <Loading/>
+            </div>
+            );
+        } else {
+       return (
+           <HashRouter>
+               <div className={styles.App}>
+                   <Navbar dayOfWeek={this.state.dayOfWeek}/>
+                   <Route path="/monday"
+                          render={() => <Monday/>}/>
+                   <Route path="/tuesday"
+                          render={() =>
+                              <Tuesday loading={this.state.loading}/>}/>
+               </div>
+           </HashRouter>)
+       }
     };
 }
 
---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Loading.module.css"
+
+class Loading extends React.Component {
+
+    render = () => {
+        return (
+                <div className={styles.loader}>
+
+                </div>
+        );
+    };
+
+}
+
+export default Loading;
